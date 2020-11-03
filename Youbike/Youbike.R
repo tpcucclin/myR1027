5119c19cb736435c63970f7a1149c0f562501355
Ching-Chang Lin <cclin@im.tpcu.edu.tw>
2020-11-03 16:24:02 +0800

    add Taipei Station

--- Youbike/Youbike.R
+++ Youbike/Youbike.R
@@ -1,7 +1,21 @@
 setwd("C:/myR1027/Youbike")
 install.packages('jsonlite')
 library(jsonlite)
-download.file("https://quality.data.gov.tw/dq_download_json.php?nid=1230
-26&md5_url=4d8de527a0bcd8a7b1aeae91120f021d","ubikeNTP.json")
+file <- "https://quality.data.gov.tw/dq_download_json.php?nid=123026&md5_url=4d8de527a0bcd8a7b1aeae91120f021d"
+download.file(file,"ubikeNTP.json")
 jdata <- fromJSON("ubikeNTP.json")
-table(jdata$sarea)
+str(jdata)
+jdata$sarea
+x=table(jdata$sarea)
+y=sort(x,decreasing = TRUE)
+z=as.data.frame(y)
+summary(z$Fr)
+boxplot(z$Freq)
+
+install.packages('data.table')
+library(data.table)
+file <- "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
+download.file(file,"ubikeTP.json")
+m <- fromJSON("ubikeTP.json")
+n <- rbindlist(m$retVal)
+
