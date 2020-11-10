79ac02ca3bc877359a95ffb6f8da853f280cfffc
tpcucclin <cclin@im.tpcu.edu.tw>
2020-11-10 14:35:49 +0800

    Update Youbike.R
    add geom_bar plot

--- Youbike/Youbike.R
+++ Youbike/Youbike.R
@@ -15,6 +15,19 @@ boxplot(z$Freq)
 
 install.packages('data.table')
 library(data.table)
+ubike.csv <- 'youbike.csv'
+url <- 'https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/csv/file'
+download.file(url,ubike.csv)
+ubike <- fread(ubike.csv,header = T,encoding = 'UTF-8',stringsAsFactors = F,colClasses = list(character=1:14))
+
+install.packages('ggplot2')
+ggplot(sarea,aes(x=sarea,y=tot)) + geom_bar(stat = "identity") +
+ theme_bw() +
+ theme(text = element_text(size=14, family="Noto Sans CJK
+TC"),axis.text.x=element_text(size=rel(1.3), angle=90,vjust = 0.5)) +
+ scale_x_discrete(name="新北市",guide = guide_axis(n.dodge = 1)) +
+ scale_y_continuous(name="總停車格")
+
 file <- "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
 download.file(file,"ubikeTP.json")
 m <- fromJSON("ubikeTP.json")
