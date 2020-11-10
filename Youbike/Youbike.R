837b4891817c0d50f0102d21fee6690b1ca1b007
tpcucclin <cclin@im.tpcu.edu.tw>
2020-11-10 14:38:19 +0800

    Update Youbike.R
    update

--- Youbike/Youbike.R
+++ Youbike/Youbike.R
@@ -21,11 +21,14 @@ download.file(url,ubike.csv)
 ubike <- fread(ubike.csv,header = T,encoding = 'UTF-8',stringsAsFactors = F,colClasses = list(character=1:14))
 
 install.packages('ggplot2')
+# 新北市有多少個 youbike 租借站? 各區的 youbike 總停車格數量為？ 最多站位於何區?
+sarea = ubike %>% group_by(sarea) %>% summarise(tot=sum(tot)) %>% arrange(tot)
+sarea$sarea <- factor(sarea$sarea,levels=sarea$sarea)
 ggplot(sarea,aes(x=sarea,y=tot)) + geom_bar(stat = "identity") +
  theme_bw() +
  theme(text = element_text(size=14, family="Noto Sans CJK
-TC"),axis.text.x=element_text(size=rel(1.3), angle=90,vjust = 0.5)) +
- scale_x_discrete(name="新北市",guide = guide_axis(n.dodge = 1)) +
+TC"),axis.text.x=element_text(size=rel(1.1), angle=0)) +
+ scale_x_discrete(name="新北市",guide = guide_axis(n.dodge = 3)) +
  scale_y_continuous(name="總停車格")
 
 file <- "https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json"
